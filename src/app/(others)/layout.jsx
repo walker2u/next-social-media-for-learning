cf80feb01ae353cc40bd0d5853298f23e6539a75
walker2u <kumar99prasad@gmail.com>
2025-04-07 21:23:29 +0530

    added basic pages

--- src/app/(others)/layout.jsx
+++ src/app/(others)/layout.jsx
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import ".././globals.css";
+import LeftSidebar from "../components/LeftSidebar";
+import RightSidebar from "../components/RightSidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,16 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex justify-between max-w-6xl mx-auto">
+          <div className="hidden sm:inline border-r h-screen sticky top-0">
+            <LeftSidebar />
+          </div>
+
+          <div className="w-2xl flex-1">{children}</div>
+          <div className="lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]">
+            <RightSidebar />
+          </div>
+        </div>
       </body>
     </html>
   );
---
+++ src/app/(others)/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='text-red-600'>Home</div>
+  )
+}
+
+export default Home
